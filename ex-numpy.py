edfc1f4a854a43adaf31c218dd36e3dfe468eaa8
Allan <allanfelipedk@gmail.com>
2025-05-29 13:47:17 -0300

    feat: changing file to .ipynb and creating graphs with matplotlib library

--- ex-numpy.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-url = "https://raw.githubusercontent.com/allanspadini/numpy/dados/citrus.csv"
-
-data = np.loadtxt(url,delimiter=",",usecols=np.arange(1,6,1),skiprows=1)
-print(data)
